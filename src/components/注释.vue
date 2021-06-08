70ac75ad8b3cc7ae9d765ee29f36969e572fca71
chenyang549 <76673327+chenyang549@users.noreply.github.com>
2021-06-08 10:07:26 +0800

    项目起步 - 删除默认代码

---
+++ src/components/注释.vue
@@ -0,0 +1,24 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+// 查看本地git状态
+// git status
+// 查看提交的记录信息
+// git log 
+// git log --oneline 简单查看
+// 将文件提交到本地暂存区
+// git add.
+// 记录此次更改
+// git commit -m"注释"
+// 提交到远程仓库
+// git push
+
+}
+</script>
+
+<style>
+
+</style>
