947b5c70d97f6c12f5776bc5618ecbe9d239e7ab
chenyang549 <76673327+chenyang549@users.noreply.github.com>
2021-06-08 10:01:42 +0800

    哈哈

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <tt/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "@/components/Helloworld.vue"
+import tt from './components/tt.vue'
+
+// 导入子组件
+// import 组件名 from  "路径"
+// @就是代表了src路径
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    tt
   }
 }
+
+
 </script>
 
 <style>
+/* 如何导入刚才的css
+注意：@在style中的使用需要加一个~@ */
+@import url(~@/assets/base.css);
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
